47dff58b0c0acf0050a1bb744db61943f05474d9
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-12-07 13:27:51 -0300

    fixing css

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -11,11 +11,14 @@ function App() {
       </div>
       <div className='BodyApp'>
         <h3 className='titulo1'>Seja bem-vindo(a)!</h3>
-        <p className='paragrafo1'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non enim semper, rhoncus arcu id, volutpat nunc. Proin ac tempus odio. Ut accumsan pellentesque libero. Praesent placerat quam non iaculis hendrerit. Etiam ut velit mauris. Pellentesque auctor, eros vel semper dictum, dui urna dictum ante, vel accumsan elit sem nec odio. Pellentesque vehicula dolor non quam gravida fringilla. Aenean facilisis vel purus non tincidunt. Etiam vel fermentum velit. Ut et sodales purus. In blandit ipsum vel metus egestas, ut tempor eros pharetra. Praesent vel velit nunc. Suspendisse nec nisl eu justo accumsan congue. Donec et urna ullamcorper, fermentum erat quis, tincidunt odio. Sed ullamcorper maximus faucibus.
-        </p>
-        <img className='gigi' src={exemplo} alt='Foto da Giovana'></img>
-        <span className='Helvetica'>@chochinfang</span>
+        <div className='meio'>
+          <p className='paragrafo1'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non enim semper, rhoncus arcu id, volutpat nunc. Proin ac tempus odio. Ut accumsan pellentesque libero. Praesent placerat quam non iaculis hendrerit. Etiam ut velit mauris. Pellentesque auctor, eros vel semper dictum, dui urna dictum ante, vel accumsan elit sem nec odio. Pellentesque vehicula dolor non quam gravida fringilla. Aenean facilisis vel purus non tincidunt. Etiam vel fermentum velis.
+          </p>
+          <img className='gigi' src={exemplo} alt='Foto da Giovana'></img>
+        </div>
+        
+        <span className='Helvetica'>@giovanachinfang</span>
       </div>
     </div>
   )
