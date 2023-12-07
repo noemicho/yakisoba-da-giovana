67c717b8f5ff8920482b005dfa12edb16110d9b7
Noemi Cho de Almeida <noemicho14@gmail.com>
2023-12-07 10:23:30 -0300

    new pages: first home page (App)

---
+++ frontend/src/pages/App.jsx
@@ -0,0 +1,18 @@
+import '../pages/App.jsx'
+
+import Header from '../components/Header'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='HeaderApp'>
+        <Header></Header>
+      </div>
+      <div className='BodyApp'>
+            aaaa
+      </div>
+    </div>
+  )
+}
+
+export default App
